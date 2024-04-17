20b3c44230892d89b0cd85bc4f8139b0864784c0
Igor Maciaszczyk <135438945+Shinivoa@users.noreply.github.com>
2024-04-17 16:23:20 +0200

    Create apps.js

---
+++ apps.js
@@ -0,0 +1,8 @@
+//select DOC
+const image = document.querySelector(".image");
+
+window.addEventListener("scroll", appearOnscroll);
+
+function appearOnscroll() {
+  console.log("YES VERY MUCH");
+}
