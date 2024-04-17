cacd7962033ec0f49882908fa74a44e6e5fd4ac2
Igor Maciaszczyk <igor.maciaszczyk@web.de>
2024-04-17 16:31:45 +0200

    accidently uploaded a duplicate

--- apps.js
+++
@@ -1,8 +0,0 @@
-//select DOC
-const image = document.querySelector(".image");
-
-window.addEventListener("scroll", appearOnscroll);
-
-function appearOnscroll() {
-  console.log("YES VERY MUCH");
-}
