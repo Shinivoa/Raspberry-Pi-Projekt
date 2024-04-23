4b1b361ff2b9379488a616caa6a1cc5ab339a580
Igor Maciaszczyk <135438945+Shinivoa@users.noreply.github.com>
2024-04-23 16:14:00 +0200

    update on some improved features and settings

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,19 @@
 //select DOC
-const image = document.querySelector(".image");
-
-
-window.addEventListener("scroll", appearOnscroll);
-
-function appearOnscroll() {
-  console.log("PLEASE WORK");
+function checkSlide() {
+  const slideIns = document.querySelectorAll(".slide-in");
+  slideIns.forEach((slideIn) => {
+    // Halfway through the image
+    const slideInAt = window.scrollY + window.innerHeight - slideIn.height / 2;
+    // Bottom of the image
+    const imageBottom = slideIn.offsetTop + slideIn.height;
+    const isHalfShown = slideInAt > slideIn.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      slideIn.classList.add("active");
+    } else {
+      slideIn.classList.remove("active");
+    }
+  });
 }
+
+var l = window.addEventListener("scroll", checkSlide);
